87293d62a549b0c6e4fa5858e9e77abfe2ac2199
ramintasalina <brigita.salina@gmail.com>
2023-03-16 07:33:18 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,49 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+    }
+}
